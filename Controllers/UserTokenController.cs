673685bba45639758f00e6e1177dfacac03fa88b
ChrisNTR <chrisntr@gmail.com>
2021-05-02 23:56:20 -0400

    Updating core Calling app

--- Controllers/UserTokenController.cs
+++ Controllers/UserTokenController.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Communication;
-using Azure.Communication.Administration;
-using Azure.Communication.Administration.Models;
+using Azure.Communication.Identity;
+using Azure.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace Calling
 {
-    [Route("/userToken")]
     public class UserTokenController : Controller
     {
         private readonly CommunicationIdentityClient _client;
@@ -26,19 +25,59 @@ namespace Calling
         /// Gets a token to be used to initalize the call client
         /// </summary>
         /// <returns></returns>
+        [Route("/token")]
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
             try
             {
-                Response<CommunicationUser> userResponse = await _client.CreateUserAsync();
-                CommunicationUser user = userResponse.Value;
-                Response<CommunicationUserToken> tokenResponse =
-                    await _client.IssueTokenAsync(user, scopes: new[] { CommunicationTokenScope.VoIP });
-                string token = tokenResponse.Value.Token;
-                DateTimeOffset expiresOn = tokenResponse.Value.ExpiresOn;
-                return this.Ok(tokenResponse);
-            } 
+                Response<(CommunicationUserIdentifier User, AccessToken Token)> response = await _client.CreateUserWithTokenAsync(scopes: new[] { CommunicationTokenScope.VoIP });
+
+                var responseValue = response.Value;
+
+                var jsonFormattedUser = new
+                {
+                    communicationUserId = responseValue.User.Id
+                };
+
+                var clientResponse = new
+                {
+                    user = jsonFormattedUser,
+                    token = responseValue.Token.Token,
+                    expiresOn = responseValue.Token.ExpiresOn
+                };
+                
+                return this.Ok(clientResponse);
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine($"Error occured while Generating Token: {ex}");
+                return this.Ok(this.Json(ex));
+            }
+        }
+
+        /// <summary>
+        /// Gets a token to be used to initalize the call client
+        /// </summary>
+        /// <returns></returns>
+        [Route("/refreshToken/{identity}")]
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(string identity)
+        {
+            try
+            {
+                CommunicationUserIdentifier identifier = new CommunicationUserIdentifier(identity);
+                Response<AccessToken> response = await _client.IssueTokenAsync(identifier, scopes: new[] { CommunicationTokenScope.VoIP });
+
+                var responseValue = response.Value;
+                var clientResponse = new
+                {
+                    token = responseValue.Token,
+                    expiresOn = responseValue.ExpiresOn
+                };
+
+                return this.Ok(clientResponse);
+            }
             catch (RequestFailedException ex)
             {
                 Console.WriteLine($"Error occured while Generating Token: {ex}");
